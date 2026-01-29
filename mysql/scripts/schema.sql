e5b613d37452d91d24ecf17d7b4e00c28cd2a01b
User <kbandari292@gmail.com>
2026-01-29 18:23:06 -0500

    functions

--- mysql/scripts/schema.sql
+++ mysql/scripts/schema.sql
@@ -5,9 +5,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     amount INT,
     description VARCHAR(255),
-    
 );
-
 
 CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
 GRANT ALL ON transactions.* TO 'expense'@'%';
